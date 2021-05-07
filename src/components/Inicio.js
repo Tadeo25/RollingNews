12ac313470ea9d58848e93b48adf396893cd2b99
Marcelo Frias <frias.marcelo@gmail.com>
2021-05-06 22:47:04 -0300

    agregadas cards para noticias princpales

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,54 +1,84 @@
-import React from 'react';
-import {Container, Carousel, Card, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Carousel, Card, Row, Col } from "react-bootstrap";
 // import Carousel from 'react-bootstrap/Carousel'
 // import Card from 'react-bootstrap/Card'
- import destacado1 from './img/vacunas_principal.jpg'
- import destacado2 from './img/estacionDeServicio_principal.jpg'
- import '../App.css';
+import destacado1 from "./img/vacunas_principal.jpg";
+import destacado2 from "./img/estacionDeServicio_principal.jpg";
+import "../App.css";
 
- 
 const Inicio = () => {
-    return (
-        <Container>
-                        <Row>
-                        <Col xs={12} md={2}>
-                            <h3>titulo</h3>
-                            </Col>
-            <Col xs={12} md={6}>
-            <Card className='shadow'>
-                <Card.Body>
-    <Carousel fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={destacado1}
-      alt="Primera Noticia"
-    />
-     <Card.Title className='bg-primary text-light'>Titulo Noticia 1 </Card.Title>
-    
-    
-        </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={destacado2}
-      alt="Second slide"
-    />    
-  </Carousel.Item>
- </Carousel>
-   
-  </Card.Body>
-</Card>
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} md={1}>
+          <h3>titulo</h3>
+        </Col>
+        <Col xs={12} md={7}>
+          <Card className="shadow rounded">
+            <Card.Header>
+              <img
+                className="d-block w-100 rounded"
+                src={destacado1}
+                alt="Imagen noticia principal"
+              />
+            </Card.Header>
+            <Card.Body className="bg-primary text-light">
+              <Card.Title>Titulo Noticia 1 </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} md={4}>
+          <h3>titulo3</h3>
+        </Col> 
+        
+        <Col xs={12} md={4}>
+        <Card className="shadow mt-4">
+            <Card.Header>
+              <img
+                className="d-block w-100"
+                src={destacado1}
+                alt="Imagen noticia principal"
+              />
+            </Card.Header>
+            <Card.Body className="bg-primary text-light">
+              <Card.Title>Titulo Noticia 1 </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
 
-    </Col>
-    <Col xs={12} md={4}>
-        <h3>titulo3</h3>
-                            </Col>
+        <Col xs={12} md={4}>
+        <Card className="shadow mt-4">
+            <Card.Header>
+              <img
+                className="d-block w-100"
+                src={destacado1}
+                alt="Imagen noticia principal"
+              />
+            </Card.Header>
+            <Card.Body className="bgcard text-light">
+              <Card.Title>Titulo Noticia 1 </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
 
-            </Row>
-          
-        </Container>
-    );
+        <Col xs={12} md={4}>
+        <Card className="shadow mt-4">
+            <Card.Header>
+              <img
+                className="d-block w-100"
+                src={destacado1}
+                alt="Imagen noticia principal"
+              />
+            </Card.Header>
+            <Card.Body className="bg-primary text-light">
+              <Card.Title>Titulo Noticia 1 </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        
+      </Row>
+    </Container>
+  );
 };
 
 export default Inicio;
