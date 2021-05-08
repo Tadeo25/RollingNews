7f096d80f72de9c68a8345ab1d8e9d05b963b0d0
Marcelo Frias <frias.marcelo@gmail.com>
2021-05-07 21:05:44 -0300

    se mejoró el diseño de las cards y se agregó categoría deportes

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Carousel, Card, Row, Col } from "react-bootstrap";
+import { Container, Carousel, Card, Row, Col, Button } from "react-bootstrap";
 // import Carousel from 'react-bootstrap/Carousel'
 // import Card from 'react-bootstrap/Card'
 import destacado1 from "./img/vacunas_principal.jpg";
@@ -8,76 +8,111 @@ import "../App.css";
 
 const Inicio = () => {
   return (
-    <Container>
-      <Row>
+
+<section className='fondo'>
+                 <Container>
+           
+             <Row>
         <Col xs={12} md={1}>
-          <h3>titulo</h3>
-        </Col>
+                 </Col>
         <Col xs={12} md={7}>
-          <Card className="shadow rounded">
-            <Card.Header>
-              <img
-                className="d-block w-100 rounded"
-                src={destacado1}
-                alt="Imagen noticia principal"
-              />
-            </Card.Header>
-            <Card.Body className="bg-primary text-light">
-              <Card.Title>Titulo Noticia 1 </Card.Title>
+          
+            <Card.Img className="rounded efectocard"src={destacado2}
+                alt="Imagen noticia principal"/>
+              
+            
+            <Card.Body className="shadow bgcard colortext rounded border">
+              <Card.Title className="font-weight-bold fuentetitulos"><h4>Titulo Noticia 1</h4> </Card.Title>
             </Card.Body>
-          </Card>
+          
         </Col>
         <Col xs={12} md={4}>
-          <h3>titulo3</h3>
+          <h3>ACA VA LA PUBLICIDAD</h3>
         </Col> 
         
         <Col xs={12} md={4}>
-        <Card className="shadow mt-4">
-            <Card.Header>
-              <img
-                className="d-block w-100"
-                src={destacado1}
-                alt="Imagen noticia principal"
-              />
-            </Card.Header>
-            <Card.Body className="bg-primary text-light">
-              <Card.Title>Titulo Noticia 1 </Card.Title>
+        <Card.Img className="rounded border-bottom mt-4"src={destacado1}
+                alt="Imagen noticia principal"/>
+              
+            
+            <Card.Body className="shadow bgcard colortext rounded border">
+              <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
             </Card.Body>
-          </Card>
+                 </Col>
+
+        <Col xs={12} md={4}>
+        <Card.Img className="rounded border-bottom mt-4"src={destacado1}
+                alt="Imagen noticia principal"/>
+              
+            
+            <Card.Body className="shadow bgcard colortext rounded border">
+              <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+            </Card.Body>
         </Col>
 
         <Col xs={12} md={4}>
-        <Card className="shadow mt-4">
-            <Card.Header>
-              <img
-                className="d-block w-100"
-                src={destacado1}
-                alt="Imagen noticia principal"
-              />
-            </Card.Header>
-            <Card.Body className="bgcard text-light">
-              <Card.Title>Titulo Noticia 1 </Card.Title>
+        <Card.Img className="rounded border-bottom mt-4"src={destacado1}
+                alt="Imagen noticia principal"/>
+              
+            
+            <Card.Body className="shadow bgcard colortext rounded border">
+              <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
             </Card.Body>
-          </Card>
-        </Col>
-
-        <Col xs={12} md={4}>
-        <Card className="shadow mt-4">
-            <Card.Header>
-              <img
-                className="d-block w-100"
-                src={destacado1}
-                alt="Imagen noticia principal"
-              />
-            </Card.Header>
-            <Card.Body className="bg-primary text-light">
-              <Card.Title>Titulo Noticia 1 </Card.Title>
-            </Card.Body>
-          </Card>
         </Col>
         
       </Row>
+      <hr></hr>
+      <h1 className='fuentetitulos'>Deportes</h1>
+        <hr></hr>
+        <Row>
+    <Col xs={12} md={6}>
+      
+    <Row className='no-gutters'>
+  <Col md={6} lg={6}  >
+  <Card.Img variant="top" src={destacado2} />
+  </Col>
+  <Col>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+      </Card.Body>
+  </Col>
+  </Row>
+        </Col>
+
+        <Col xs={12} md={6}>
+      
+    <Row className='no-gutters'>
+  <Col md={6} lg={6}  >
+  <Card.Img variant="top" src={destacado2} />
+  </Col>
+  <Col>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+      </Card.Body>
+  </Col>
+  </Row>
+        </Col>
+    </Row>
     </Container>
+    
+    </section>
   );
 };
 
