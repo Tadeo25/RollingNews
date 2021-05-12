c20430c9ce27e68db8efdf5e216bd7ea562613c7
Tadeo25 <juan_tadeo25@hotmail.com>
2021-05-12 10:48:43 -0300

    ffirst commit

---
+++ src/components/DetalleNoticias.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function DetalleNoticias() {
+    return (
+        <div>
+            <h1 className="jumbotron">
+                Titulo Noticia
+            </h1>
+        </div>
+    )
+}
