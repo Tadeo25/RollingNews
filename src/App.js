a924145733c5ab70af1cf98398914aa85d9af3b6
Marcelo Frias <frias.marcelo@gmail.com>
2021-05-28 16:53:23 -0300

    navbar, inicio y suscripcion funcionales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css' ;
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navegacion from './components/comun/Navegacion';
-import Inicio from './components/Inicio';
-import SeccionNoticias from './components/SeccionNoticias'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navegacion from "./components/common/Navegacion";
+import Inicio from "./components/Inicio";
+//import SeccionNoticias from './components/SeccionNoticias'
+import Suscripcion from "./components/Suscripcion";
 
 function App() {
   return (
     <Router>
       <Navegacion></Navegacion>
       <Switch>
-        <Route exact path='/' >
-        <Inicio></Inicio>
+        <Route exact path="/">
+          <Inicio></Inicio>
         </Route>
-        <Route exact path='/Seccion'>
-          <SeccionNoticias></SeccionNoticias>
+        <Route exact path="/Seccion">
+          {/* <SeccionNoticias></SeccionNoticias> */}
         </Route>
-        <Route exact path=''></Route>
-        <Route exact path=''></Route>
+        <Route exact path="/suscripcion">
+          <Suscripcion></Suscripcion>
+        </Route>
+        <Route exact path=""></Route>
       </Switch>
     </Router>
   );
