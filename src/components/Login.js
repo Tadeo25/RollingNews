3416fb9de45299bd2c70ca9dded0874ff0762f78
Graciela Tabera <gra.tavi@gmail.com>
2021-05-30 13:44:32 -0300

    se dio formato al codigo

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,34 +2,30 @@ import React from 'react';
 import {Form,Button,Container,Row,Col} from 'react-bootstrap';
 
 const Login = () => {
-    return ( 
-      <Container>
-        <Row>
-          <Col md={{span:6,offset:3}} >
-        <Form className= 'my-4'>
-          <h1 className='text-center fuentetitulos text-dark mb-4'>Iniciar Sesion</h1>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email</Form.Label>
-    <Form.Control type="email" placeholder="Ingrese su Email" />
-  </Form.Group>
- 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Contraseña</Form.Label>
-    <Form.Control type="password" placeholder="Contraseña"/>
-  </Form.Group>
-  <Form.Text className=""> Olvido su contraseña?
-    </Form.Text>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Ingresar
-  </Button>
-</Form>
-</Col>
-</Row>
-</Container>
-    );
+  return ( 
+    <Container>
+      <Row>
+        <Col md={{span:6,offset:3}}>
+          <Form className='my-4'>
+            <h1 className='text-center fuentetitulos text-dark mb-4'>Iniciar Sesion</h1>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Ingrese su Email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Contraseña"/>
+            </Form.Group>
+            <Form.Text className=""> Olvido su contraseña?</Form.Text>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out"/>
+            </Form.Group>
+            <Button variant="primary" type="submit">Ingresar</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
