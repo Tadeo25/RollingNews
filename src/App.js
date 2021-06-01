286fe8976ff9d74ee4e433aba44b0dbe5f4cfdb4
Marisol Fiorella <marisolfiorellasan@gmail.com>
2021-06-01 19:32:47 -0300

    Footer creado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navegacion from "./components/common/Navegacion";
+import Footer from "./components/common/Footer";
 import Inicio from "./components/Inicio";
 import SeccionNoticias from './components/SeccionNoticia'
 import Suscripcion from "./components/Suscripcion";
@@ -49,6 +50,7 @@ function App() {
         </Route>
         <Route exact path=""></Route>
       </Switch>
+      <Footer></Footer>
     </Router>
   );
 }
