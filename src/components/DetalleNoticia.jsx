75a8bb55720a979d038d574707a1c4c91ce62a10
Marcelo Frias <frias.marcelo@gmail.com>
2021-06-01 23:32:05 -0300

    se modificó el código para hacer el contenido responsive

--- src/components/DetalleNoticia.jsx
+++ src/components/DetalleNoticia.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 
 export default function DetalleNoticias() {
 
 return (
     <div>
-      <div className="m-5 p-2">
+      <Row>
+        <Col xs={12} md={10}>
+        <div className="m-5 p-2">
       <h5>Seccion</h5>
       <h1>Titulo Noticia</h1>
       <p>
@@ -13,12 +16,18 @@ return (
       </p>
       <img 
       src="https://www.viewsonic.com/skin/frontend/smartwave/viewsonic/images/category-pages/projector-home/4k_projectors/Ultra_HD.jpg" 
-      alt="" 
+      alt="" className="img-fluid" 
       />
        <p>
           lorem loremlorem ipsum dolor sit amet, consectetur lorem, sed diam lorem sed lorem
       </p>
       </div>
+      </Col>
+      <Col xs={12} md={2}>
+      </Col>
+      </Row>
+      
+      
     </div>
   );
 }
